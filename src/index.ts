c728fe118564db3e1af9489abf524e468c503d1b
odair-pedroso <odairpedroso90@gmail.com>
2022-06-25 16:54:20 -0300

    queue

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { loadEnvs } from './tools/envs.tools';
 loadEnvs(`${__dirname}/../envs/.env-${process.env.NODE_ENV}`);
-import './server';
+import './queues';
 
 process.on('uncaughtException', (error: Error) => {
   console.error(`uncaughtException ${error.message}`);
