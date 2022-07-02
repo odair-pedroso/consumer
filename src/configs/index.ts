acb03c3f8976ea7bcebf68d568ffb0444c52356c
odair-pedroso <odairpedroso90@gmail.com>
2022-07-02 11:01:48 -0300

    criacao fila email

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,7 +1,22 @@
 export default {
-  redis:{
-    host:process.env.REDIS_HOST!,
-    password:process.env.REDIS_PASSWORD!,
+  redis: {
+    host: process.env.REDIS_HOST!,
+    password: process.env.REDIS_PASSWORD!,
     port: Number(process.env.REDIS_PORT!),
   },
+  mail: {
+    config: {
+      host: process.env.MAIL_HOST,
+      port: Number(process.env.MAIL_PORT),
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+      },
+
+    },
+    default: {
+      from: process.env.MAIL_FROM,
+      to: process.env.MAIL_TO,
+    }
+  }
 };
